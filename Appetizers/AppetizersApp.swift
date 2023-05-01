cff08f8d6c6197accc8936b8620256bcd21854a4
Farangis <Farangis>
2023-05-01 10:03:27 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Махмадёрова Фарангис Шухратовна on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
